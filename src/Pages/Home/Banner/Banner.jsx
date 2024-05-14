8746c1a64de75ff5d448f6acca7dbdf17cc704ca
dronatoo <mrdronatoo@gmail.com>
2024-05-14 12:25:30 -0700

    completed banner

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -3,14 +3,14 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 const Banner = () => {
     return (
         <div>
-            <Carousel >
-                <div>
-                    <img src="https://i.ibb.co/XbQr2Rn/ramen.jpg" />
+            <Carousel>
+                <div className="md:h-[90vh]">
+                    <img src="https://i.ibb.co/grkjwLB/steak.jpg" />
                 </div>
-                <div>
+                <div className="md:h-[90vh]">
                     <img src="https://i.ibb.co/3F0XW3r/pizza.png" />
                 </div>
-                <div>
+                <div className="md:h-[90vh]">
                     <img src="https://i.ibb.co/F6yWcft/chicken.png" />
                 </div>
             </Carousel>
