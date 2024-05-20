e6bc25004ccca581c142c438a12fea43245b5e47
dronatoo <mrdronatoo@gmail.com>
2024-05-20 13:34:17 -0700

    completed featured section responsive

--- src/Components/SectionTitle/SectionTitle.jsx
+++ src/Components/SectionTitle/SectionTitle.jsx
@@ -1,9 +1,9 @@
 const SectionTitle = ({title}) => {
     return (
         <div className="flex items-center gap-2 justify-center mb-8">
-            <hr className="border-b-1 border-gray-300 w-[200px]"/>
-            <h1 className="text-xl font-bold">{title}</h1>
-            <hr className="border-b-1 border-gray-300 w-[200px]"/>
+            <hr className="border-b-1 border-gray-300 w-[60px] md:w-[200px]"/>
+            <h1 className="text-sm md:text-xl font-bold">{title}</h1>
+            <hr className="border-b-1 border-gray-300 w-[60px] md:w-[200px]"/>
         </div>
     );
 };
--- src/Pages/Home/Featured_products/FeaturedProducts.jsx
+++ src/Pages/Home/Featured_products/FeaturedProducts.jsx
@@ -4,21 +4,21 @@ import dessert from "../../../../public/banner logos/dessert-banner.png"
 
 const FeaturedProducts = () => {
     return (
-        <section className="my-14 lg:mx-5 mx-10">
+        <section className="my-14 lg:mx-5 md:mx-10 mx-3.5">
             <SectionTitle title={"Featured Products"}/>
             <div className="flex lg:flex-row flex-col gap-2 lg:gap-7 md:gap-2 lg:h-[430px]">
                 <div className="flex flex-col justify-between flex-1">
                     <div>
-                        <img className="lg:hidden rounded-lg h-[120px] md:h-[150px] lg:h-[430px] w-full" src="https://i.ibb.co/Cb5BdY9/Thank-you-for-your-understanng.png" />
-                        <img className="hidden lg:block rounded-lg h-[120px] md:h-[150px] lg:h-[430px] w-full" src="https://i.ibb.co/Cb5BdY9/Thank-you-for-your-understanng.png" />
+                        <img className="lg:hidden rounded-lg h-[100px] md:h-[150px] lg:h-[430px] w-full" src="https://kfcbd.com/storage/sliders/VM81uJ1eknFIbYdWrebEJ4ALb.jpg" />
+                        <img className="hidden lg:block rounded-lg h-[100px] md:h-[150px] lg:h-[430px] w-full" src="https://i.ibb.co/Cb5BdY9/Thank-you-for-your-understanng.png" />
                     </div>
                 </div>
                 <div className="flex flex-col gap-2 justify-between flex-1">
                     <div>
-                        <img className="h-[120px] md:h-[150px] lg:h-[200px] w-full rounded-lg" src={dessert} alt="Dessert"/>
+                        <img className="h-[100px] md:h-[150px] lg:h-[200px] w-full rounded-lg" src={dessert} alt="Dessert"/>
                     </div>
                     <div>
-                        <img className="h-[120px] md:h-[150px] lg:h-[200px] w-full rounded-lg" src={drink} alt="Drink"/>
+                        <img className="h-[100px] md:h-[150px] lg:h-[200px] w-full rounded-lg" src={drink} alt="Drink"/>
                     </div>
                 </div>
             </div>
