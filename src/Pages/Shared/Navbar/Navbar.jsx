e9c1fba7a3ffcb7e9b6e731e696abf322198473e
dronatoo <mrdronatoo@gmail.com>
2024-05-26 16:46:27 -0700

    changed login button

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -5,11 +5,11 @@ import { Link, NavLink } from "react-router-dom";
 const Navbar = () => {
     const Links = <>
         <NavLink to="/"><li className="text-base font-semibold hover:text-yellow-400">Home</li></NavLink>
-        <NavLink><li className="text-base font-semibold hover:text-yellow-400">About</li></NavLink>
         <NavLink><li className="text-base font-semibold hover:text-yellow-400">Menu</li></NavLink>
         <NavLink><li className="text-base font-semibold hover:text-yellow-400">Dashboard</li></NavLink>
         <NavLink><li className="text-base font-semibold hover:text-yellow-400">Cart</li></NavLink>
-        <NavLink><li className="text-base font-semibold hover:text-yellow-400">Login</li></NavLink>
+        <NavLink><li className="text-base font-semibold hover:text-yellow-400">About</li></NavLink>
+        <NavLink><li className="md:btn md:btn-sm md:bg-transparent md:hover:bg-transparent border-3 md:text-white border-white bg text-base font-semibold hover:text-yellow-400">Login</li></NavLink>
     </>
     return (
         <div className="md:rounded-md text-white bg-black fixed bg-opacity-30 z-10 container mx-auto md:py-2 px-5">
@@ -20,7 +20,7 @@ const Navbar = () => {
                 <h1 className="ml-2 md:ml-4 text-lg md:text-xl font-bold">My <span className="text-yellow-400">Res</span>tau<span className="text-yellow-400">rant</span></h1>
               </div>
               <div className="flex-none">
-                <ul className="menu menu-horizontal px-1 hidden md:flex gap-5">
+                <ul className="menu menu-horizontal px-1 hidden md:flex gap-5 items-center">
                   {Links}
                 </ul>
               </div>
