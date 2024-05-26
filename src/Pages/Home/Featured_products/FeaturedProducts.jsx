630a98a2851c214f553f34e291e61e1bfc9833cf
dronatoo <mrdronatoo@gmail.com>
2024-05-26 15:01:07 -0700

    added featured banners

--- src/Pages/Home/Featured_products/FeaturedProducts.jsx
+++ src/Pages/Home/Featured_products/FeaturedProducts.jsx
@@ -1,6 +1,7 @@
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
-import drink from "../../../assets/banner logos/drink-banner.png"
-import dessert from "../../../assets/banner logos/dessert-banner.png"
+import pizza from "../../../assets/featured/pizza.png"
+import burger from "../../../assets/featured/burger.png"
+import korean from "../../../assets/featured/korean.png"
 
 const FeaturedProducts = () => {
     return (
@@ -9,16 +10,16 @@ const FeaturedProducts = () => {
             <div className="flex lg:flex-row flex-col gap-2 lg:gap-7 md:gap-2 lg:h-[430px]">
                 <div className="flex flex-col justify-between flex-1">
                     <div>
-                        <img className="lg:hidden rounded-lg h-[100px] md:h-[150px] lg:h-[430px] w-full" src="https://kfcbd.com/storage/sliders/VM81uJ1eknFIbYdWrebEJ4ALb.jpg" />
-                        <img className="hidden lg:block rounded-lg h-[100px] md:h-[150px] lg:h-[430px] w-full" src="https://i.ibb.co/Cb5BdY9/Thank-you-for-your-understanng.png" />
+                        <img className="lg:hidden rounded-lg h-[100px] md:h-[150px] lg:h-[430px] w-full" src={burger} />
+                        <img className="hidden lg:block rounded-lg h-[100px] md:h-[150px] lg:h-[430px] w-full" src={burger} />
                     </div>
                 </div>
                 <div className="flex flex-col gap-2 justify-between flex-1">
                     <div>
-                        <img className="h-[100px] md:h-[150px] lg:h-[200px] w-full rounded-lg" src={dessert} alt="Dessert"/>
+                        <img className="h-[100px] md:h-[150px] lg:h-[200px] w-full rounded-lg" src={pizza} alt="Dessert"/>
                     </div>
                     <div>
-                        <img className="h-[100px] md:h-[150px] lg:h-[200px] w-full rounded-lg" src={drink} alt="Drink"/>
+                        <img className="h-[100px] md:h-[150px] lg:h-[200px] w-full rounded-lg" src={korean} alt="Drink"/>
                     </div>
                 </div>
             </div>
