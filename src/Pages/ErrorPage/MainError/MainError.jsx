2c446e0013db2aa7d70785901fce6cdf292578b0
dronatoo <mrdronatoo@gmail.com>
2024-06-08 16:40:15 +0600

    created error page

--- src/Pages/ErrorPage/MainError/MainError.jsx
+++ src/Pages/ErrorPage/MainError/MainError.jsx
@@ -1,7 +1,14 @@
+import Lottie from "lottie-react";
+import { Link } from "react-router-dom";
+import ErrorAnimation from '../../../../public/404.json'
+
 const MainError = () => {
     return (
-        <div>
-            error
+        <div className="min-h-screen">
+            <div className="text-center py-28">
+            <Link to="/"><button className="btn bg-gray-800 text-white rounded-xl my-10">Go To Home</button></Link>
+            <Lottie className="w-8/12 lg:w-1/3 mx-auto" animationData={ErrorAnimation}/>
+        </div>
         </div>
     );
 };
