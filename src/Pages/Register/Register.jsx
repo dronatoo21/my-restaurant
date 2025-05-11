7e2fbb9a222ce25cecd91c65298a758c14def408
dronatoo <mrdronatoo@gmail.com>
2025-05-12 02:15:15 +0600

    checking

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -25,7 +25,7 @@ const Register = () => {
        
     const handleRegister = e => {
         e.preventDefault();
-        const name = e.target.name.value;
+        const name = e.target.name.value; 
         const email = e.target.email.value;
         const password = e.target.password.value;
         const checked = e.target.checkbox.checked;
